5d457cb2fc93a71abb00f16e4e4a67961e0e206f
Alfonso <benbanda07@gmail.com>
2020-05-10 23:55:56 -0500

    Initial Commit

---
+++ c-course/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  c-course
+//
+//  Created by Alfonso Banda on 10/05/20.
+//  Copyright © 2020 Alfonso Banda. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
